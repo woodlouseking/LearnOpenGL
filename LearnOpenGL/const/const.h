bb8d351e4f9e80efaa3368604a275d54de05c896
woodlouse <330279121@qq.com>
2019-11-03 21:46:59 +0800

    Add triangle

--- LearnOpenGL/const/const.h
+++ LearnOpenGL/const/const.h
@@ -10,9 +10,26 @@
 #define const_h
 
 namespace LEARN_OPEN_GL {
+    const int LOG_INFO_LEN = 1024;
+
     const int SCR_WIDTH = 800;
     const int SCR_HEIGHT = 600;
     const char SCR_TLE[] = "LearnOpenGL";
+    const float vertices[] = {
+        -0.5f, -0.5f, 0.0f, // 左下角
+        0.5f, -0.5f, 0.0f, // 右下角
+        0.0f, 0.5f, 0.0f //顶角
+    };
+    const char *vertexShaderSource = "#version 330 core\n"
+                "layout (location=0) in vec3 aPos;\n"
+                "void main(){\n"
+                "       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0f);\n"
+                "}\n\0";
+    const char *fragmentShaderSource = "#version 330 core\n"
+                "out vec4 fragColor;\n"
+                "void main(){\n"
+                "   fragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                "}\n\0";
 }
 
 #endif /* const_h */
--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -6,10 +6,10 @@
 //  Copyright © 2019年 woodlouse. All rights reserved.
 //
 
-#include <iostream>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include "const/const.h"
+#include <iostream>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -44,13 +44,71 @@ int main()
         std::cout<<"GLAD FAIL"<<std::endl;
         return -1;
     }
-    
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    // 顶点着色器的处理
+    GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &LEARN_OPEN_GL::vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+    int success;
+    char infoLog[LEARN_OPEN_GL::LOG_INFO_LEN];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        // 获取编译信息
+        glGetShaderInfoLog(vertexShader, LEARN_OPEN_GL::LOG_INFO_LEN, NULL, infoLog);
+        std::cout<<"Compile vertex shader failed : info "<<infoLog<<std::endl;
+        return -1;
+    }
+    
+    // 片段着色器的编译
+    GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &LEARN_OPEN_GL::fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, LEARN_OPEN_GL::LOG_INFO_LEN, NULL, infoLog);
+        std::cout<<"Compile fragment shader failed:info"<<infoLog<<std::endl;
+        return -1;
+    }
+    
+    // 着色器程序
+    GLuint shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetShaderiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(shaderProgram, LEARN_OPEN_GL::LOG_INFO_LEN, NULL, infoLog);
+        std::cout<<"Link shader failed : info " <<infoLog<<std::endl;
+        return -1;
+    }
+    
+    
+    // 三角形顶点的处理
+    GLuint VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER,
+                 sizeof(LEARN_OPEN_GL::vertices),
+                 LEARN_OPEN_GL::vertices,
+                 GL_STATIC_DRAW);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, sizeof(GLfloat), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
     
     while (!glfwWindowShouldClose(window)) {
         processInput(window);
         glClearColor(0.5f, 0.2, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+        
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         
         glfwSwapBuffers(window);
         glfwPollEvents();
